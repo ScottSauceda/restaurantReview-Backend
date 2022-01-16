8420b1fda378579c9b5eb9ef983233ce4fbc6c73
Marcus Sauceda <marcussauceda@Marcuss-iMac.local>
2022-01-16 15:38:55 -0600

    first commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/restaurants", restaurants)
+app.use("/", restaurants)
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
 
 export default app
